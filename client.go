7eace4dfecddb1e00167d522a5920f42e0303e02
YumuXie <98315777+YumuXie@users.noreply.github.com>
2022-10-25 16:43:28 +0100

    Add files via upload

---
+++ client.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"net/rpc"
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+
+	//	"bufio"
+	//	"os"
+	"fmt"
+)
+
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
+	flag.Parse()
+	fmt.Println("Server: ", *server)
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+
+	request := stubs.Request{Message: "Hello"}
+	response := new(stubs.Response)
+	client.Call(stubs.PremiumReverseHandler, request, response)
+	fmt.Println("Responded: " + response.Message)
+	//TODO: connect to the RPC server and send the request(s)
+}
